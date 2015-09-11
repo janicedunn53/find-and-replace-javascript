87a6a9d7b0a32c41ce39a9b3fb498cf664c53c5d
Janice Dunn <janicedunn53@gmail.com>
2015-09-11 12:38:57 -0700

    Add front-end using jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,17 @@ var findAndReplace = function(sentence, oldWord, newWord) {
   var newSentence = sentence.replace(re, newWord);
   return newSentence;
 };
+
+$(document).ready(function() {
+  $("form#user-inputs").submit(function(event) {
+    var sentence = $("input#sentence").val();
+    var oldWord = $("input#oldWord").val();
+    var newWord = $("input#newWord").val();
+    var newSentence = findAndReplace(sentence, oldWord, newWord);
+
+    $(".newSentence").text(newSentence);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
